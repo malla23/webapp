57d467833018b27defe012c1d61a074162b93948
malla23 <binod.malla@outlook.com>
2020-11-04 00:15:51 +1100

    Second commit

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
 	echo"<h1> Welcome to the first CI/CD pipeline </h1>";
+	
 
+	echo"<br>";
+	echo </br>";
 	echo"Page under Maintenance.....";
-
+	echo"<p> For more information please contact ..... </p> ";
 ?>
