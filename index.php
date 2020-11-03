bbbf8b9aea6a425b9fbe77daa321bc4be18d213a
malla23 <binod.malla@outlook.com>
2020-11-04 00:24:56 +1100

    Third commit

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 	echo"<h1> Welcome to the first CI/CD pipeline </h1>";
 	
 
-	echo"<br>";
-	echo </br>";
+
 	echo"Page under Maintenance.....";
 	echo"<p> For more information please contact ..... </p> ";
 ?>
